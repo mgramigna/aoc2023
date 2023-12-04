dd992157fba72104d68e766a943b48945c52a191
mgramigna <matthew.gramigna@patinahealth.com>
2023-12-04 13:19:03 -0500

    day 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,3 +1,36 @@
+fn part_one() -> usize {
+    let lines: Vec<&str> = include_str!("../inputs/1-1.txt").lines().collect();
+
+    let sum: usize = lines
+        .iter()
+        .map(|line| {
+            let first_digit_char = line
+                .chars()
+                .find(|c| c.is_numeric())
+                .expect("Could not locate a numeric character in line");
+
+            let last_digit_char = line
+                .chars()
+                .rfind(|c| c.is_numeric())
+                .expect("Could not locate a numeric character in line");
+
+            let digit_string = format!("{}{}", first_digit_char, last_digit_char);
+
+            return digit_string
+                .parse::<usize>()
+                .expect("Could not parse int from string");
+        })
+        .sum();
+
+    sum
+}
+
+// Too tired to do this rn
+fn part_two() -> usize {
+    0
+}
+
 fn main() {
-    println!("Hello world");
+    println!("Part 1: {}", part_one());
+    println!("Part 2: {}", part_two());
 }
